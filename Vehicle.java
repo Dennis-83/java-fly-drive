10ae424f015b44198e5667e49ec5985b2f862eae
Dennis-83 <dodo_ez@hotmail.com>
2023-07-21 16:22:58 +0200

    Added abstract Vehicle class

---
+++ Vehicle.java
@@ -0,0 +1,16 @@
+public abstract class Vehicle {
+
+    public void speed(int speed) {
+        System.out.println("This vehicle is going " + speed + "kph");
+    }
+    public void weight(float weight) {
+        System.out.println("This vehicle weights " + weight + "kg");
+    }
+    public void startEngine(){
+        System.out.println("You turn on the engine");
+    }
+    public void turnOffEngine() {
+        System.out.println("You turn off the engine");
+    }
+
+}
